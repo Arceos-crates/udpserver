c80adbdc73618aafbb38ce2fcad276bb26656350
xushanpu123 <798304030@qq.com>
2024-05-01 17:37:56 +0800

    udpserverCI

---
+++ readme.md
@@ -0,0 +1,6 @@
+支持arceos执行的最小模块，运行命令：
+
+```shell
+CURRENT_DIR=`pwd` make A=apps/net/udpserver ARCH=riscv64 NET=y LOG=info  run 
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//! Simple HTTP server.
+//!
+//! Benchmark with [Apache HTTP server benchmarking tool](https://httpd.apache.org/docs/2.4/programs/ab.html):
+//!
+//! ```
+//! ab -n 5000 -c 20 http://X.X.X.X:5555/
+//! ```
+
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
 
@@ -5,34 +13,84 @@
 #[cfg(feature = "axstd")]
 extern crate axstd as std;
 
-use std::io;
-use std::net::{ToSocketAddrs, UdpSocket};
+use std::io::{self, prelude::*};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 const LOCAL_IP: &str = "0.0.0.0";
 const LOCAL_PORT: u16 = 5555;
 
-fn receive_loop() -> io::Result<()> {
-    let addr = (LOCAL_IP, LOCAL_PORT).to_socket_addrs()?.next().unwrap();
-    let socket = UdpSocket::bind(addr)?;
-    println!("listen on: {}", socket.local_addr().unwrap());
-    let mut buf = [0u8; 1024];
+macro_rules! header {
+    () => {
+        "\
+HTTP/1.1 200 OK\r\n\
+Content-Type: text/html\r\n\
+Content-Length: {}\r\n\
+Connection: close\r\n\
+\r\n\
+{}"
+    };
+}
+
+const CONTENT: &str = r#"<html>
+<head>
+  <title>Hello, ArceOS</title>
+</head>
+<body>
+  <center>
+    <h1>Hello, <a href="https://github.com/rcore-os/arceos">ArceOS</a></h1>
+  </center>
+  <hr>
+  <center>
+    <i>Powered by <a href="https://github.com/rcore-os/arceos/tree/main/apps/net/httpserver">ArceOS example HTTP server</a> v0.1.0</i>
+  </center>
+</body>
+</html>
+"#;
+
+macro_rules! info {
+    ($($arg:tt)*) => {
+        match option_env!("LOG") {
+            Some("info") | Some("debug") | Some("trace") => {
+                print!("[INFO] {}\n", format_args!($($arg)*));
+            }
+            _ => {}
+        }
+    };
+}
+
+fn http_server(mut stream: TcpStream) -> io::Result<()> {
+    let mut buf = [0u8; 4096];
+    let _len = stream.read(&mut buf)?;
+
+    let response = format!(header!(), CONTENT.len(), CONTENT);
+    stream.write_all(response.as_bytes())?;
+
+    Ok(())
+}
+
+fn accept_loop() -> io::Result<()> {
+    let listener = TcpListener::bind((LOCAL_IP, LOCAL_PORT))?;
+    println!("listen on: http://{}/", listener.local_addr().unwrap());
+
+    let mut i = 0;
     loop {
-        match socket.recv_from(&mut buf) {
-            Ok((size, addr)) => {
-                println!("recv: {}Bytes from {}", size, addr);
-                let mid = core::str::from_utf8(&buf).unwrap();
-                println!("{}", mid);
-                let mid = ["response_", mid].join("");
-                socket.send_to(mid.as_bytes(), addr)?;
-                buf = [0u8; 1024];
+        match listener.accept() {
+            Ok((stream, addr)) => {
+                info!("new client {}: {}", i, addr);
+                thread::spawn(move || match http_server(stream) {
+                    Err(e) => info!("client connection error: {:?}", e),
+                    Ok(()) => info!("client {} closed successfully", i),
+                });
             }
             Err(e) => return Err(e),
-        };
+        }
+        i += 1;
     }
 }
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    println!("Hello, simple udp client!");
-    receive_loop().expect("test udp client failed");
+    println!("Hello, ArceOS HTTP server!");
+    accept_loop().expect("test HTTP server failed");
 }
